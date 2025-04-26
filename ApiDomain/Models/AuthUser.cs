0ba6972f37b016039e38f541a8a560cbc520e7e6
11qfour <futurenature31@gmail.com>
2025-04-26 19:03:41 +0300

    add auth models

---
+++ ApiDomain/Models/AuthUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace ApiDomain.Models
+{
+    public class AuthUser //модель пользователя
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; } // Ссылка на Customer
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "User"; // по умолчанию обычный пользователь
+        public void HashPassword(string password)
+        {
+            PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(password, 13);
+        }
+
+        // Проверка пароля
+        public bool VerifyPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, PasswordHash);
+        }
+    }
+}
---
+++ ApiDomain/Models/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiDomain.Models
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public Guid AuthUserId { get; set; }
+        public AuthUser AuthUser { get; set; } = null!;
+    }
+}
